bf9bf9ca76db7bf7d9970e7257a2f8474bd070e3
Bailey Herbert <hello@bailey.sh>
2021-10-26 13:17:37 -0700

    Improve test bootstrap docs

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,12 +4,13 @@ require __DIR__ . '/../vendor/autoload.php';
 
 // Configure an app for testing
 // Register one here: https://build.envato.com/my-apps/
-define('TEST_OAUTH_SECRET', 'UeITwZSUjY8i1PpTgBUPqZRJXObDB7JX');
+define('TEST_OAUTH_SECRET', 'Enter a client secret here for testing');
 define('TEST_OAUTH_REDIRECT', 'https://localhost/');
-define('TEST_OAUTH_CLIENT', 'oauth-test-hctds8wd');
+define('TEST_OAUTH_CLIENT', 'oauth-client-id123456');
 
+// Create a token with the OAuth app above and paste it here
 define('TEST_OAUTH_CODE', '');
 
 // Configure a personal token for testing
 // Register one here: https://build.envato.com/my-apps/
-define('TEST_PERSONAL_TOKEN', 'zImroz44UrMDJcglOmLGKiALa3wrjdf6');
+define('TEST_PERSONAL_TOKEN', 'Enter a personal token here for testing');
